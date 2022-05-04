cdcaea7c448f22988e2662c195b9d109d59655bc
HongJinHyeok <redmax45@naver.com>
2022-05-04 23:25:35 +0900

    ✨ create ec2

---
+++ demo-2/instance.tf
@@ -0,0 +1,15 @@
+
+
+
+
+# http://cloud-images.ubuntu.com/locator/ec2/
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0933d0fdfb094bfd1"
+  instance_type = "t2.micro"
+}
+
