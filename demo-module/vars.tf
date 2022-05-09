f6778bc0eaa73455d3d1e907007513eb9c1d110a
HongJinHyeok <redmax45@naver.com>
2022-05-09 21:29:21 +0900

    ✨ terraform variables

---
+++ demo-module/vars.tf
@@ -0,0 +1,11 @@
+variable "AWS_REGION" {
+  default = "ap-northeast-2"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "~/.ssh/id_rsa"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "~/.ssh/id_rsa.pub"
+}
