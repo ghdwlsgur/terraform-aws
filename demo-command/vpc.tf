374b6ac5184c706c456eaeb031cbde421bbcd9e5
HongJinHyeok <redmax45@naver.com>
2022-05-13 22:27:47 +0900

    ✨ aws defined subnet attributes

---
+++ demo-command/vpc.tf
@@ -0,0 +1,35 @@
+# Internet VPC
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = "true"
+  enablc_dns_hostnames = "true"
+  enable_classiclink   = "false"
+  tags = {
+    Name = "main"
+  }
+}
+
+# Subnets
+resource "aws_subnet" "main-public-1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "ap-northeast-2a"
+
+  tags = {
+    Name = "main-public-1"
+  }
+}
+
+resource "aws_subnet" "main-public-2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "ap-northeast-2c"
+
+  tags = {
+    Name = "main-public-2"
+  }
+}
