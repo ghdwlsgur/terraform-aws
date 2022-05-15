78f6c89424b8b63c164c6dfa1908e164e38e1d2b
HongJinHyeok <redmax45@naver.com>
2022-05-15 21:46:34 +0900

    ✅ 주석 추가

--- ec2-parameter-store-terraform/main.tf
+++ ec2-parameter-store-terraform/main.tf
@@ -27,10 +27,11 @@ resource "aws_key_pair" "web_admin" {
   public_key = data.aws_ssm_parameter.secret.value
   # pem 저장
   provisioner "local-exec" {
-    command = "echo '${aws_key_pair.web_admin.key_name}' > ~/redmax45/aws-key-pair/web_admin.pem"
+    command = "echo '${aws_key_pair.web_admin.key_name}' >> ~/redmax45/aws-key-pair/web_admin.pem"
   }
 }
 
+# cat ~/.ssh/id_rsa.pub | pbcopy 
 data "aws_ssm_parameter" "secret" {
   name = "/terraform/ssh-key"
 }
@@ -60,18 +61,22 @@ resource "aws_instance" "web" {
     aws_security_group.ssh.id,
     data.aws_security_group.default.id
   ]
+
+  tags = {
+    Name = "Terraform's EC2"
+  }
 }
 
-resource "aws_db_instance" "web_db" {
-  allocated_storage   = 8                                     # 할당할 용량 (기가바이트)
-  engine              = "mysql"                               # 데이터베이스 엔진
-  engine_version      = "8.0.28"                              # 데이터베이스 엔진 버전
-  instance_class      = "db.t2.micro"                         # 인스턴스 타입
-  username            = "admin"                               # 계정 이름
-  password            = data.aws_ssm_parameter.password.value # 계정 비밀번호
-  skip_final_snapshot = true
-}
+# resource "aws_db_instance" "web_db" {
+#   allocated_storage   = 8                                     # 할당할 용량 (기가바이트)
+#   engine              = "mysql"                               # 데이터베이스 엔진
+#   engine_version      = "8.0.28"                              # 데이터베이스 엔진 버전
+#   instance_class      = "db.t2.micro"                         # 인스턴스 타입
+#   username            = "admin"                               # 계정 이름
+#   password            = data.aws_ssm_parameter.password.value # 계정 비밀번호
+#   skip_final_snapshot = true
+# }
 
-data "aws_ssm_parameter" "password" {
-  name = "/terraform/rds-password"
-}
+# data "aws_ssm_parameter" "password" {
+#   name = "/terraform/rds-password"
+# }
