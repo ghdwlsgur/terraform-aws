69ab4e0f7ecdadbb4235b272172fbf9b61ab74f6
HongJinHyeok <redmax45@naver.com>
2022-05-22 18:50:10 +0900

    🐛  add private key.pem code

--- provision-ec2-kali-linux/outputs.tf
+++ provision-ec2-kali-linux/outputs.tf
@@ -1,3 +1,10 @@
+// private key (pem)
+output "ssh_private_key" {
+  value     = tls_private_key.tls.private_key_pem
+  sensitive = true
+}
+
+
 // connect command
 output "ssh_ec2_kali_linux_connect" {
   value = "ssh -v -i ~/.ssh/kali.pem -o IdentitiesOnly=yes kali@${aws_instance.kali_linux.public_ip}"
