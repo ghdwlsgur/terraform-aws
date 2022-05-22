3314536234d8ce28d4eed41acc333048e4cbb035
HongJinHyeok <redmax45@naver.com>
2022-05-22 19:34:52 +0900

    ✅ add outbound rule of sg

--- provision-ec2-kali-linux/securitygroup.tf
+++ provision-ec2-kali-linux/securitygroup.tf
@@ -15,6 +15,13 @@ resource "aws_security_group" "kali_security" {
     protocol    = "tcp"
     cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
