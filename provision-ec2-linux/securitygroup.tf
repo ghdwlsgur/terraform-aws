90e85651d6d89d06079b01ba5dcf31afeda3b4ba
HongJinHyeok <redmax45@naver.com>
2022-05-30 21:36:30 +0900

    ♻️ ec2 sg

---
+++ provision-ec2-linux/securitygroup.tf
@@ -0,0 +1,23 @@
+
+data "aws_security_group" "default" {
+  name = "default"
+}
+
+resource "aws_security_group" "linux_security" {
+  name        = "linux_security"
+  description = "Allow SSH port from anywhere"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
