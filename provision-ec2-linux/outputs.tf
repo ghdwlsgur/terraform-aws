f0c0ede3d1295a970260bb87dce5c75e0bc836d9
HongJinHyeok <redmax45@naver.com>
2022-05-30 21:36:20 +0900

    ♻️ get private key

---
+++ provision-ec2-linux/outputs.tf
@@ -0,0 +1,8 @@
+output "ssh_private_key" {
+  value     = tls_private_key.tls.private_key_pem
+  sensitive = true
+}
+
+output "ssh_ec2_linux_connect" {
+  value = "ssh -v -i ~/.ssh/linux.pem -o IdentitiesOnly=yes ec2-user@${aws_instance.linux.public_ip}"
+}
