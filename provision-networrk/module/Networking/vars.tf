2191f2faeb0ea4955a3559c9a95d0eb09502f29f
HongJinHyeok <redmax45@naver.com>
2022-07-01 15:25:00 +0900

    ✅ terraform variables

---
+++ provision-networrk/module/Networking/vars.tf
@@ -0,0 +1,29 @@
+variable "environment" {
+  description = "Deployment Environment"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block of the vpc"
+}
+
+variable "public_subnets_cidr" {
+  type        = list(any)
+  description = "CIDR block for Public Subnet"
+}
+
+variable "private_subnets_cidr" {
+  type        = list(any)
+  description = "CIDR block for Private Subnet"
+}
+
+variable "region" {
+  description = "Region in which the bastion host will be launched"
+  default     = "ap-southeast-1"
+}
+
+variable "availability_zones" {
+  type        = list(any)
+  description = "AZ in which all the resources will be deployed"
+}
+
+
