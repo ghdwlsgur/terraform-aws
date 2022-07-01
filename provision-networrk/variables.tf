e68c511def933687c2de5cee6a92225b82f7f107
HongJinHyeok <redmax45@naver.com>
2022-07-01 15:24:40 +0900

    ✅ 네트워크 모듈 설정

---
+++ provision-networrk/variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  type        = string
+  description = "Singapore"
+  default     = "ap-southeast-1"
+}
+
+variable "environment" {
+  description = "Deployment Environment"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block of the vpc"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets_cidr" {
+  type        = list(any)
+  description = "CIDR block for Public Subnet"
+  default     = ["10.0.1.0/24"]
+}
+
+variable "private_subnets_cidr" {
+  type        = list(any)
+  description = "CIDR block for Private Subnet"
+  default     = ["10.0.10.0/24"]
+}
+
