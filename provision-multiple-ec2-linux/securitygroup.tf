63a932b5e77fb21736e77fabd718c4b89019af60
HongJinHyeok <redmax45@naver.com>
2022-07-02 16:32:10 +0900

    🐛 다중 vpc 환경에서 default sg 설정 및 vpc 지정

--- provision-multiple-ec2-linux/securitygroup.tf
+++ provision-multiple-ec2-linux/securitygroup.tf
@@ -1,15 +1,26 @@
 
+# VPC Securigy Group 지정 
 data "aws_security_group" "default" {
-  name = "default"
+  # name = "default"
+  filter {
+    name   = "tag:Name"
+    values = ["custom-VPC-SG"]
+  }
 }
 
 data "http" "myip" {
   url = "http://ipv4.icanhazip.com"
 }
 
+data "aws_ssm_parameter" "vpc-id" {
+  name = "/terraform/vpc-id"
+}
+
+# 보안그룹이 생성될 vpc의 id 추가
 resource "aws_security_group" "linux_security" {
   name        = "linux_security"
   description = "Allow SSH port from anywhere"
+  vpc_id      = data.aws_ssm_parameter.vpc-id.value
   ingress {
     from_port   = 22
     to_port     = 22
@@ -18,9 +29,9 @@ resource "aws_security_group" "linux_security" {
   }
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
