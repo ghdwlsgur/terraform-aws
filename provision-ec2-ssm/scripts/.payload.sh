4c85bb2b06a8ac44bf7a0bb7b00bd5afff5c133c
HongJinHyeok <vjinhyeokv@gmail.com>
2022-08-09 02:32:37 +0900

    ssm agent download

---
+++ provision-ec2-ssm/scripts/.payload.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash 
+
+yum update -y 
+sudo curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm" -o "session-manager-plugin.rpm"
+sudo yum install -y session-manager-plugin.rpm
